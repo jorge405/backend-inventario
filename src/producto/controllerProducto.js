54d1f46f324090618070c0dcd463defea9230d39
jorge405 <jorgepomsjhope7@gmail.com>
2025-05-14 18:22:13 -0400

    agregando complementos de productos

--- src/producto/controllerProducto.js
+++ src/producto/controllerProducto.js
@@ -5,7 +5,7 @@ import {pool} from '../DB/conecction.js'
 export const getProducto= async (req,res)=>{
     
     try {
-        const [rows]= await pool.query('select p.nombre_cientifico,p.nombre_comercial,p.contenido,p.medicion,p.fecha_vencimiento,p.precio_unit,l.laboratorio,pr.presentacion,u.uso,p.estado_int from producto_medicamento as p INNER JOIN laboratorio as l on p.cod_laboratorio=l.cod_laboratorio INNER JOIN presentacion as pr on p.cod_presentacion=pr.cod_presentacion INNER JOIN uso as u on p.cod_uso=u.cod_uso WHERE p.estado_int=1 ')    
+        const [rows]= await pool.query('select p.cod_producto,p.nombre_cientifico,p.nombre_comercial,p.contenido,p.medicion,p.fecha_vencimiento,p.precio_unit,l.laboratorio,pr.presentacion,u.uso,p.estado_int from producto_medicamento as p INNER JOIN laboratorio as l on p.cod_laboratorio=l.cod_laboratorio INNER JOIN presentacion as pr on p.cod_presentacion=pr.cod_presentacion INNER JOIN uso as u on p.cod_uso=u.cod_uso WHERE p.estado_int=1 ')    
         if (rows) {
             res.status(200).json({
                 msg:'ok',
@@ -44,6 +44,79 @@ export const addProducto= async (req,res)=>{
         res.status(500).json({
             msg:'error en el servidor',
             status:'500',
+        })
+    }
+}
+
+export const addLaboratorio= async(req,res)=>{
+    try {
+        const {laboratorio} = req.body
+        const [rows]= await pool.query('insert into laboratorio(laboratorio) values (?)',[laboratorio])
+        if (rows.affectedRows===1) {
+            res.status(200).json({
+                msg:'creado',
+                status:'200'
+            })
+        }else{
+            res.status(204).json({
+                msg:'no creado',
+                status:'204'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg:'error en el servidor',
+            status:'500'
+        })
+    }
+}
+
+export const addPresentacion = async(req,res)=>{
+    try {
+        const {presentacion} = req.body;
+        const [rows]= await pool.query('insert into presentacion(presentacion) values(?)',[presentacion])
+        if (rows.affectedRows===1) {
+            res.status(200).json({
+                msg:'creado',
+                status:'200'
+            })
+        }else{
+            res.status(204).json({
+                msg:'no creado',
+                status:'204'
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg:'error en el servidor',
+            status:'500'
+        })
+    }
+}
+
+export const addUso= async(req,res)=>{
+    try {
+        const {uso}=req.body;
+        const [rows] = await pool.query('insert into uso(uso) values(?)',[uso])
+        if (rows.affectedRows===1) {
+            res.status(200).json({
+                msg:'creado',
+                status:'200'
+            })
+        }else{
+            res.status(204).json({
+                msg:'no creado',
+                status:'204'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg:'error en el servidor',
+            status:'500'
         })
     }
 }
--- src/producto/routerProducto.js
+++ src/producto/routerProducto.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import { verifyToken } from '../utils/auth.js'
-import {getProducto,addProducto} from '../producto/controllerProducto.js'
+import {getProducto,addProducto,addLaboratorio,addPresentacion,addUso} from '../producto/controllerProducto.js'
 
 const routerProducto= express.Router();
 
 
 routerProducto.get('/getProducto',verifyToken,getProducto)
 routerProducto.post('/addProducto',verifyToken,addProducto)
+// adicionales a producto
+routerProducto.post('/addLaboratorio',verifyToken,addLaboratorio)
+routerProducto.post('/addPresentacion',verifyToken,addPresentacion)
+routerProducto.post('/addUso',verifyToken,addUso)
+
 
 export default routerProducto;
